57fd351ade2c6aeca5ce0cb720b8c90ddf053770
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-03-27 12:41:10 +0500

    Initial commit

--- app.js
+++ app.js
@@ -22,3 +22,17 @@ const Promise3 = (value) => {
         }, 1000);
     });
 };
+
+// Promise1(5)
+//     .then((result1) => {       
+//         return Promise2(result1);
+//     })
+//     .then((result2) => {
+//         return Promise3(result2);
+//     })
+//     .then((finalResult) => {
+//         console.log("Final Value:", finalResult);
+//     })
+//     .catch((error) => {
+//         console.log("Error:", error);
+//     });
